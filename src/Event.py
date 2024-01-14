a993e27be261d21e8f2bfa4f2da9c9ba0e5c981e
Dave Ackley <ackley@livingcomputation.org>
2024-01-14 04:14:02 -0700

    Populate

---
+++ src/Event.py
@@ -0,0 +1,145 @@
+from abc import ABC, abstractmethod
+from functools import total_ordering
+import threading
+import subprocess
+import gc
+
+@total_ordering
+class Event(ABC):
+    
+    classmap = {}
+    objcount = 0
+
+    @classmethod
+    def registerSubclass(cls,key,func):
+        cls.classmap[key] = func
+
+    @classmethod
+    def makeEvent(cls,mrs,list):
+        try:
+            action, type, *args = list
+        except:
+            print(f"'{list}' too short to be an action")
+            return None
+        if action != 'action':
+            print(f"'{action}' is not 'action', in '{list}'")
+            return None
+        if type not in cls.classmap:
+            print(f"'{type} is not a registered action type, in '{list}'")
+            return None
+        #print("EVMEV",type,args)
+        return cls.classmap[type](mrs,type,*args)
+
+    def __init__(self,mrs,name):
+        self.mrs = mrs
+        self.name = name
+        self.sched = None
+        Event.objcount += 1
+        self.objnum = Event.objcount
+
+    def __lt__(self, other):
+        return self.objnum < other.objnum
+
+    def __str__(self):
+        return str(self.__class__.__module__) + "." + str(self.__class__.__name__) + "" + str(self.__dict__)
+
+    def __repr__(self):
+        return "EVT:"+"DONG"
+
+    @abstractmethod
+    def run(self,eq,now,dead):
+        pass
+
+    
+class DimScreenEvent(Event):
+    def __init__(self,mrs,name):
+        super().__init__(mrs,name)
+        self.brightness = -1    # Current brightness unset
+        self.delay = 60*60      # default seconds til dim
+        
+    def reschedule(self):
+        self.mrs.scheduleEvent(self.delay,self) # one hour on
+        
+    def wake(self):
+        self.reschedule();
+        self.setBrightnessPercent(30)
+
+    def run(self,eq,now,dead):
+        self.setBrightnessPercent(0)
+        delta = now - dead
+        print(f'{dead} executed at {now} delay {delta}')
+        start = eq.now()
+        n = gc.collect()
+        end = eq.now()
+        gctime = end - start
+        print(f'{n} objects collected in {gctime}')
+
+    def setBrightnessPercent(self,pct):
+        if self.brightness != pct: 
+            print(f'brightness to {pct}')
+            self.brightness = pct
+            decks = self.mrs.decks 
+            decks.setAllDecksBrightness(pct)
+
+
+class ClockEvent(Event):
+
+    def run(self,eq,now,dead):
+        delta = now - dead
+        print(f'{dead} executed at {now} delay {delta}')
+        then = int(now/10)*10+10
+        eq.runAt(then,self)
+
+class ThreadEvent(Event):
+
+    @abstractmethod
+    def threadRun(self,eq,now,dead):
+        pass
+
+    def run(self,eq,now,dead):
+        worker = self.threadRun
+        def catch(eq,now,dead):
+            try:
+                #print("LKASLKMRSOAART",self,now)
+                worker(eq,now,dead)
+                #print("FDDDAAANLKASLKMRSOAART",self,now,dead)
+            except Exception as e:
+                print("ZTNDKGOGN",self,e)
+        thread = threading.Thread(target=catch,args=(eq,now,dead))
+        thread.start()
+
+class SleepEvent(ThreadEvent):
+    def threadRun(self,eq,now,dead):
+        print("SLEEPEV "+str(now))
+        eq.sleep(4)
+        print("SLEEPEZ "+str(eq.now()))
+        eq.runIn(3,self)
+        
+class ShellEvent(ThreadEvent):
+
+    Event.registerSubclass('shell',lambda mrs,type,*args: ShellEvent(mrs,type,*args))
+
+    def __init__(self,mrs,name,*args):
+        super().__init__(mrs,name)
+        #print(args)
+        self.args = args
+        
+    def threadRun(self,eq,now,dead):
+        cmd = self.args[0]
+        cmda = cmd.split()
+        prog = cmda[0]
+        rprog = self.mrs.resolveProgram(prog)
+        if rprog is None:
+            print(f"WARNING: NO EXECUTABLE '{prog}' FOUND; '{cmd}' IGNORED")
+            return
+        if rprog != prog:
+            cmda[0] = rprog
+            cmd = " ".join(cmda)
+
+        print("SHELL",cmd)
+        comp=subprocess.run(cmd,shell=True, capture_output=True)
+        if comp.returncode != 0:
+            print("WARNING:",comp.args,"exited",comp.returncode)
+            print("STDOUT:",comp.stdout)
+            print("STDERR:",comp.stderr)
+
---
+++ src/MrState.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+# MrState: AKA "Shared Data Access"
+#      AKA "Mr State"
+
+import os
+import sys
+import shutil
+
+import Event
+import EventQueue
+import Config
+
+class MrState:
+    def __init__(self,name):
+        self.name = name
+        print("__mrstate__",self.name)
+        self.EQ = EventQueue.EventQueue(self,"EQ")
+        self.configPath = self.findConfigOrDie()
+        self.loadConfig()
+        sda = self.getRequiredSection('MrState');
+        self.bindirs = []       # paths in search order
+        self.bindirmap = {}     # path->True
+        if 'bindirs' in sda:
+            for id in sda['bindirs']:
+                self.addBinDir(id)
+        if len(self.bindirs) == 0:
+            self.addBinDir("")  # default path 
+
+        #e = Event.Event.makeEvent(self,['action','setimage','layoutT2sday',"t2img/foo.png"])
+        print("SLFKKLFD")
+
+    def addBinDir(self,path):
+        epath = os.path.expanduser(path)
+        if epath not in self.bindirmap:
+            self.bindirs.append(epath)
+            self.bindirmap[epath] = True
+
+    def resolveProgram(self,prog):
+        if prog[0] == '/':
+            return prog
+        for path in self.bindirs:
+            aprog = os.path.join(path,prog)
+            rprog = shutil.which(aprog)
+            if rprog is not None:
+                return rprog
+        return None
+
+    def findConfigOrDie(self):
+        paths = []
+        e = os.environ
+        ekey ='MrStateCONFIGPATH'
+        if ekey in e:           # First try environmental variable
+            paths.append(e[ekey])
+        paths.append('~/.config/MrState/MrState.cfg') # Then 'std loc'
+        paths.append('./MrState.cfg')                    # Last ditch
+        for p in paths:
+            (exists,path) = self.expandPath(p)
+            if exists:
+                print(f"Found config file {path}")
+                return path
+        self.die(f"Config file not found in {paths}")
+
+    def expandPath(self,path):
+        epath = os.path.expanduser(path)
+        if os.path.exists(epath):
+            return (True,epath)
+        return (False,None)
+
+    def die(self,message):
+        print(f"Error: {message}")
+        exit(2)
+
+    def getEventQueue(self):
+        return self.EQ
+
+    def getDeckBySerial(self,serial):
+        info = self.decks.findDeckInfoIfAny(serial) # or None
+        if info is not None:
+            return info.deck
+        return None
+
+    def getDeckInfoBySerial(self,serial):
+        return self.decks.findDeckInfoIfAny(serial) # or None
+
+    def getRequiredSection(self,name):
+        return self.config.getRequiredSection(name)
+
+    def getOptionalSection(self,name):
+        return self.config.getOptionalSection(name)
+
+    def loadConfig(self):
+        path = self.configPath
+        if os.path.isfile(path):
+            self.config = Config.Config("Cfg",path)
+        else:
+            self.die(f"Can't load {path}")
+        self.config.load()
+        self.config.print()
+
+
+    def mainLoop(self):
+        self.EQ.mainLoop()
+
+    def scheduleEvent(self,time,event):
+        self.EQ.runIn(time,event)
+
+    def eqeg(self):
+        print("hewo bongo")
+        print(self.EQ)
+        print("hewo bongo " + str(EQ))
+        print("hewo bingo " + repr(EQ))
+
+    def paneleg(self):
+        p = Panel.Panel("zongK")
+        print("sdsdkl "+repr(p))
+        s = Screen.Screen("sdklskdlds")
+        print("mahbookah "+repr(s))
+
+    def configeg():
+        c = Config.Config("Cfg","notes/foo.toml")
+        print("sdsdkl "+repr(c))
+        c.load()
+        c.print()
+
+    def run(self):
+        self.mainLoop()
+    
